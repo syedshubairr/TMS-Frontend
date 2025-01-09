9a404c7e4d241abc35c29f64e86d0957655d8b01
syedshubairr <syedshubair8299@gmail.com>
2025-01-09 13:18:27 +0500

    Added New component Tab Panel for new design of Auth Page.

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -2,46 +2,45 @@ import { useState } from "react";
 import "./styles.css";
 import LoginForm from "../../components/LoginForm";
 import RegisterForm from "../../components/RegisterForm";
+import { Box, Tab, Tabs, useTheme } from "@mui/material";
+import { TabPanel } from "../../components/TabPanel";
 
 const Auth = () => {
+  const theme = useTheme();
+  const [value, setValue] = useState(0);
   const [onRegister, setOnRegister] = useState<boolean>(false);
   const togglePanel = () => {
     setOnRegister(!onRegister);
   };
+  const handleChange = (event: any, newValue: number) => {
+    setValue(newValue);
+  };
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
   return (
     <div className="flex justify-center h-screen items-center overflow-hidden ">
       <div className="box lg:max-w-4xl">
-        <div className={`cover ${onRegister && "rotate-active"}`}>
-          <div className="front">
-            <img
-              className=""
-              src="https://projectsly.com/images/task-management-system-screenshot-1.png?v=1691124479409199525"
-              alt=""
-            />
-            <div className="text">
-              <span className="text-1">
-                Success is built upon well organized Tasks.
-              </span>
-              <span className="text-2 text-xs">Let's get connected</span>
-            </div>
-          </div>
-          <div className="back">
-            <img
-              src="https://projectsly.com/images/blog/benefits-of-employee-task-management-system.png?v=1686553999071005322"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="forms h-full">
-          <div className="form-content h-full">
-            <div className="login-form">
-              <LoginForm togglePanel={togglePanel} />
-            </div>
-            <div className="signup-form">
-              <RegisterForm togglePanel={togglePanel} />
-            </div>
-          </div>
-        </div>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="fullWidth"
+            aria-label="Login and Register Tabs"
+          >
+            <Tab label="Login" {...a11yProps(0)} />
+            <Tab label="Register" {...a11yProps(1)} />
+          </Tabs>
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            <LoginForm togglePanel={togglePanel} />
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            <RegisterForm togglePanel={togglePanel} />
+          </TabPanel>
+        </Box>
       </div>
     </div>
   );
