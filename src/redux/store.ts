92092865ced5a8a49bab0e7a0da7a794aed8d958
syedshubairr <syedshubair8299@gmail.com>
2025-01-15 14:39:42 +0500

    Added base code for Redux toolkit, and created API client.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,11 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { thunk } from "redux-thunk";
 
 const rootReducer = combineReducers({});
 
-const store = configureStore({
+export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
