14414940d77a5e1f4fda48faadb352bdded1d8fc
syedshubairr <syedshubair8299@gmail.com>
2025-01-18 21:29:56 +0500

    Submission Service done.

---
+++ src/services/SubmissionService/index.ts
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { api, setAuthHeader } from "../apiClient";
+
+export const submitTask = createAsyncThunk(
+  "submission/submitTask",
+  async ({ taskId, githubLink }: { taskId: number; githubLink: string }) => {
+    setAuthHeader(localStorage.getItem("jwt"), api);
+    try {
+      const { data } = await api.post(
+        `/api/submission?task_id=${taskId}&github_link=${githubLink}`,
+        {}
+      );
+      console.log("Submitted Task", data);
+      return data;
+    } catch (error: any) {
+      console.log("error: ", error);
+      throw Error(error.response.data.error);
+    }
+  }
+);
+export const fetchAllSubmissions = createAsyncThunk(
+  "submission/fetchAllSubmissions",
+  async () => {
+    setAuthHeader(localStorage.getItem("jwt"), api);
+    try {
+      const { data } = await api.get("/api/submission");
+      console.log("Submitted Task", data);
+      return data;
+    } catch (error: any) {
+      console.log("error: ", error);
+      throw Error(error.response.data.error);
+    }
+  }
+);
+export const fetchSubmissionByTaskId = createAsyncThunk(
+  "submission/fetchSubmissionByTaskId",
+  async ({ taskId }: { taskId: number }) => {
+    setAuthHeader(localStorage.getItem("jwt"), api);
+    try {
+      const { data } = await api.get(`/api/submission/task/${taskId}`);
+      console.log("Fetch Task by id", data);
+      return data;
+    } catch (error: any) {
+      console.log("error: ", error);
+      throw Error(error.response.data.error);
+    }
+  }
+);
+export const acceptDeclineSubmission = createAsyncThunk(
+  "submission/acceptDeclineSubmission",
+  async ({ taskId, status }: { taskId: number; status: boolean }) => {
+    setAuthHeader(localStorage.getItem("jwt"), api);
+    try {
+      const { data } = await api.put(
+        `/api/submission/task/${taskId}?status=${status}`
+      );
+      console.log("accept or Decline Submission", data);
+      return data;
+    } catch (error: any) {
+      console.log("error: ", error);
+      throw Error(error.response.data.error);
+    }
+  }
+);
